1e4ef0aa04feb1f21abab4136e909cbf16b0066b
Yonas Mekonnen Feshaye <yonas.m.feshaye@hotmail.com>
2023-09-24 22:47:13 -0400

    Fourth commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,41 +3,91 @@ import Image from 'next/image'
 
 import { useState, Fragment } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
+import { manufacturers } from '@/constants'
 
 import { SearchManufacturerProps } from '@/types'
 
-const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerProps) => {
-    const [query, setQuery] = useState('')
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState('')
+
+  const filteredManufactureres =
+    query === ""
+      ? manufacturers : manufacturers.filter((item) => (
+        item.toLowerCase()
+          .replace(/\s+/g, "")
+          .includes(query.toLowerCase().replace(/\s+/g, ""))
+      ))
+
   return (
-    <div className='seach-manufacturer'>
-        <Combobox>
-            <div className="relative w-full">
-                <Combobox.Button className="absolute top=[14px]" >
-                    <Image 
-                      src="/car-logo.svg"
-                      width={20}
-                      height={20}
-                      className="ml-4"
-                      alt="Car Logo" 
-                    />
-                </Combobox.Button>
-                <Combobox.Input 
-                  className="search-manufacturer__input"
-                  placeholder='Colkswagen'
-                  displayValue={(manufacturer: string) => manufacturer}
-                  onChange={(e) => setQuery(e.target.value)}
-                />
-                <Transition 
-                  as={Fragment}
-                  
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]" >
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder='Colkswagen'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options>
+              {filteredManufactureres.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className="search-manufacturer__option"
                 >
-
-                </Transition>
-            </div>
-        </Combobox>
-
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filteredManufactureres.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) => `
+                          relative search-manufacturer__option 
+                          ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}
+                        `}
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
+                        >
+                          {item}
+                        </span>
+                        {selected && (
+                          <span
+                            className={`absolute inset-y-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'}`}
+                          >
+                            ✓ {/* You can customize this checkmark icon */}
+                          </span>
+                        )}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
     </div>
-  )
+  );
+
 }
 
 export default SearchManufacturer
