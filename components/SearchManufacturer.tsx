c8350ee0507978fd8f8a8dbf290265bdc6ed49d1
yonasFeshaye <yonas.m.feshaye@hotmail.com>
2023-09-28 15:51:10 -0400

    Sixth Commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -33,7 +33,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
           </Combobox.Button>
           <Combobox.Input
             className="search-manufacturer__input"
-            placeholder='Colkswagen'
+            placeholder='Volkswagen'
             displayValue={(manufacturer: string) => manufacturer}
             onChange={(e) => setQuery(e.target.value)}
           />
