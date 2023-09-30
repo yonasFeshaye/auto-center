192941db1f07236cc93356e9d9b58a0f889e092a
Yonas Mekonnen Feshaye <yonas.m.feshaye@hotmail.com>
2023-09-30 16:06:50 -0400

    Final Commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -7,7 +7,7 @@ import { manufacturers } from '@/constants'
 
 import { SearchManufacturerProps } from '@/types'
 
-const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) => {
   const [query, setQuery] = useState('')
 
   const filteredManufactureres =
@@ -20,7 +20,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
   return (
     <div className='search-manufacturer'>
-      <Combobox value={manufacturer} onChange={setManufacturer}>
+      <Combobox value={selected} onChange={setSelected}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]" >
             <Image
@@ -52,7 +52,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                 >
                   Create "{query}"
                 </Combobox.Option>
-               ) : (
+              ) : (
                 filteredManufactureres.map((item) => (
                   <Combobox.Option
                     key={item}
--- types/index.ts
+++ types/index.ts
@@ -18,17 +18,17 @@ export interface SearchManufacturerProps {
 }
 
 export interface CarProps {
-    city_mpg:number;
-    class:string;
-    combination_mpg:number;
-    cylinders:number
-    displacement:number;
-    drive:string;
-    fuel_type:string;
-    highway_mpg:number;
-    make:string;
-    model:string;
-    transmission:string;
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
     year: number;
 }
 
@@ -38,4 +38,20 @@ export interface FilterProps {
     fuel: string;
     limit: number;
     model: string;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[]
+}
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+    setLimit: number
 }
